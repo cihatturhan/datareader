a7804ca31face239303aecb8245fd1cbd17aeb88
Cihat TURHAN <cihatturhan@gmail.com>
2022-12-14 23:01:19 +0100

    Initial commit

---
+++ src/main/java/com/cihatturhan/datareader/DatareaderApplication.java
@@ -0,0 +1,13 @@
+package com.cihatturhan.datareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatareaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatareaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cihatturhan/datareader/DatareaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cihatturhan.datareader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatareaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
